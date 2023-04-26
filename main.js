c1598e25799836925ed85fe55ac593bf8e28f863
Will DePue <will@depue.net>
2023-04-26 00:08:47 -0700

    In progress

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ class Miner {
   }
 
   initPipelines() {
-    this.sha256Pipeline = this.createPipeline(matMulShader, [this.u_s_BindLayout, this.r_BindLayout]);
+    this.sha256Pipeline = this.createPipeline(sha256Shader(this.device.limits.maxComputeWorkgroupSizeX), [this.u_s_BindLayout, this.r_BindLayout]);
   }
 
   createBindGroupLayout(string_entries) {
@@ -134,42 +134,137 @@ function bufferSize(dimA, dimB = 1) {
   return dimA * dimB * Float32Array.BYTES_PER_ELEMENT;
 }
 
-// Multiplies two matrices.
-// Example shader.
-const matMulShader = `
-    struct Matrix {
-        data: array<f32>,
-    }
+// Referenced from https://github.com/MarcoCiaramella/sha256-gpu/blob/main/index.js
+const sha256Shader = (maxWorkgroupX) => `
+  // SHA-256 for 32-bit aligned messages
 
-    struct Uniforms {
-      dimY: u32, // row dimension of A and row dimension of C
-      dimX: u32, // col dimension of B and col dimension of C
-      dimS: u32, // shared dimension of A and B
-    };
+  fn swap_endianess32(val: u32) -> u32 {
+      return ((val>>24u) & 0xffu) | ((val>>8u) & 0xff00u) | ((val<<8u) & 0xff0000u) | ((val<<24u) & 0xff000000u);
+  }
 
-    @group(1) @binding(0) var<storage, read> A: Matrix;
-    @group(1) @binding(1) var<storage, read> B: Matrix;
+  fn shw(x: u32, n: u32) -> u32 {
+      return (x << (n & 31u)) & 0xffffffffu;
+  }
 
-    @group(0) @binding(1) var<storage, read_write> C: Matrix;
-    @group(0) @binding(0) var<uniform> dimBuffer: Uniforms;
+  fn r(x: u32, n: u32) -> u32 {
+      return (x >> n) | shw(x, 32u - n);
+  }
 
-    @compute @workgroup_size(16, 16)
-    fn main (@builtin(global_invocation_id) global_id: vec3<u32>) {
-        let row: u32 = global_id.x;
-        let col: u32 = global_id.y;
-        let dimX: u32 = dimBuffer.dimX;
-        let dimY: u32 = dimBuffer.dimY;
-        let dimS: u32 = dimBuffer.dimS;
+  fn g0(x: u32) -> u32 {
+      return r(x, 7u) ^ r(x, 18u) ^ (x >> 3u);
+  }
 
-        if (row >= dimY || col >= dimX) {
+  fn g1(x: u32) -> u32 {
+      return r(x, 17u) ^ r(x, 19u) ^ (x >> 10u);
+  }
+
+  fn s0(x: u32) -> u32 {
+      return r(x, 2u) ^ r(x, 13u) ^ r(x, 22u);
+  }
+
+  fn s1(x: u32) -> u32 {
+      return r(x, 6u) ^ r(x, 11u) ^ r(x, 25u);
+  }
+
+  fn maj(a: u32, b: u32, c: u32) -> u32 {
+      return (a & b) ^ (a & c) ^ (b & c);
+  }
+
+  fn ch(e: u32, f: u32, g: u32) -> u32 {
+      return (e & f) ^ ((~e) & g);
+  }
+
+  @group(0) @binding(0) var<storage, read_write> messages: array<u32>;
+  @group(0) @binding(1) var<uniform> numThreads: u32;
+  @group(0) @binding(2) var<storage, read> message_sizes: array<u32>;
+  @group(0) @binding(3) var<storage, read_write> hashes: array<u32>;
+
+  @compute @workgroup_size(${maxWorkgroupX})
+  fn sha256(@builtin(global_invocation_id) global_id: vec3<u32>) {
+
+      let index = global_id.x;
+      if (index >= numThreads) {
           return;
-        }
-
-        var sum: f32 = 0.0;
-        for (var i: u32 = 0; i < dimS; i = i + 1) {
-            sum = sum + A.data[row * dimS + i] * B.data[i * dimX + col];
-        }
-
-        C.data[row * dimX + col] = sum;
       }
-  `;
+      let message_base_index = index * message_sizes[1];
+      let hash_base_index = index * (256u / 32u);
+
+      // == padding == //
+
+      messages[message_base_index + message_sizes[0]] = 0x00000080u;
+      for (var i = message_sizes[0] + 1; i < message_sizes[1] - 2; i++){
+          messages[message_base_index + i] = 0x00000000u;
+      }
+      messages[message_base_index + message_sizes[1] - 2] = 0;
+      messages[message_base_index + message_sizes[1] - 1] = swap_endianess32(message_sizes[0] * 32u);
+
+      // == processing == //
+
+      hashes[hash_base_index] = 0x6a09e667u;
+      hashes[hash_base_index + 1] = 0xbb67ae85u;
+      hashes[hash_base_index + 2] = 0x3c6ef372u;
+      hashes[hash_base_index + 3] = 0xa54ff53au;
+      hashes[hash_base_index + 4] = 0x510e527fu;
+      hashes[hash_base_index + 5] = 0x9b05688cu;
+      hashes[hash_base_index + 6] = 0x1f83d9abu;
+      hashes[hash_base_index + 7] = 0x5be0cd19u;
+
+      let k = array<u32,64>(
+          0x428a2f98u, 0x71374491u, 0xb5c0fbcfu, 0xe9b5dba5u, 0x3956c25bu, 0x59f111f1u, 0x923f82a4u, 0xab1c5ed5u,
+          0xd807aa98u, 0x12835b01u, 0x243185beu, 0x550c7dc3u, 0x72be5d74u, 0x80deb1feu, 0x9bdc06a7u, 0xc19bf174u,
+          0xe49b69c1u, 0xefbe4786u, 0x0fc19dc6u, 0x240ca1ccu, 0x2de92c6fu, 0x4a7484aau, 0x5cb0a9dcu, 0x76f988dau,
+          0x983e5152u, 0xa831c66du, 0xb00327c8u, 0xbf597fc7u, 0xc6e00bf3u, 0xd5a79147u, 0x06ca6351u, 0x14292967u,
+          0x27b70a85u, 0x2e1b2138u, 0x4d2c6dfcu, 0x53380d13u, 0x650a7354u, 0x766a0abbu, 0x81c2c92eu, 0x92722c85u,
+          0xa2bfe8a1u, 0xa81a664bu, 0xc24b8b70u, 0xc76c51a3u, 0xd192e819u, 0xd6990624u, 0xf40e3585u, 0x106aa070u,
+          0x19a4c116u, 0x1e376c08u, 0x2748774cu, 0x34b0bcb5u, 0x391c0cb3u, 0x4ed8aa4au, 0x5b9cca4fu, 0x682e6ff3u,
+          0x748f82eeu, 0x78a5636fu, 0x84c87814u, 0x8cc70208u, 0x90befffau, 0xa4506cebu, 0xbef9a3f7u, 0xc67178f2u
+      );
+
+      let num_chunks = (message_sizes[1] * 32u) / 512u;
+      for (var i = 0u; i < num_chunks; i++){
+          let chunk_index = i * (512u/32u);
+          var w = array<u32,64>();
+          for (var j = 0u; j < 16u; j++){
+              w[j] = swap_endianess32(messages[message_base_index + chunk_index + j]);
+          }
+          for (var j = 16u; j < 64u; j++){
+              w[j] = w[j - 16u] + g0(w[j - 15u]) + w[j - 7u] + g1(w[j - 2u]);
+          }
+          var a = hashes[hash_base_index];
+          var b = hashes[hash_base_index + 1];
+          var c = hashes[hash_base_index + 2];
+          var d = hashes[hash_base_index + 3];
+          var e = hashes[hash_base_index + 4];
+          var f = hashes[hash_base_index + 5];
+          var g = hashes[hash_base_index + 6];
+          var h = hashes[hash_base_index + 7];
+          for (var j = 0u; j < 64u; j++){
+              let t2 = s0(a) + maj(a, b, c);
+              let t1 = h + s1(e) + ch(e, f, g) + k[j] + w[j];
+              h = g;
+              g = f;
+              f = e;
+              e = d + t1;
+              d = c;
+              c = b;
+              b = a;
+              a = t1 + t2;
+          }
+          hashes[hash_base_index] += a;
+          hashes[hash_base_index + 1] += b;
+          hashes[hash_base_index + 2] += c;
+          hashes[hash_base_index + 3] += d;
+          hashes[hash_base_index + 4] += e;
+          hashes[hash_base_index + 5] += f;
+          hashes[hash_base_index + 6] += g;
+          hashes[hash_base_index + 7] += h;
+      }
+      hashes[hash_base_index] = swap_endianess32(hashes[hash_base_index]);
+      hashes[hash_base_index + 1] = swap_endianess32(hashes[hash_base_index + 1]);
+      hashes[hash_base_index + 2] = swap_endianess32(hashes[hash_base_index + 2]);
+      hashes[hash_base_index + 3] = swap_endianess32(hashes[hash_base_index + 3]);
+      hashes[hash_base_index + 4] = swap_endianess32(hashes[hash_base_index + 4]);
+      hashes[hash_base_index + 5] = swap_endianess32(hashes[hash_base_index + 5]);
+      hashes[hash_base_index + 6] = swap_endianess32(hashes[hash_base_index + 6]);
+      hashes[hash_base_index + 7] = swap_endianess32(hashes[hash_base_index + 7]);
+}`;
